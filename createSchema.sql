7a435be45b81b2da03c61017d0c5ff385791f3ae
egor <egorka.leps@mail.ru>
2022-07-21 05:42:01 +0700

    final veersion

--- createSchema.sql
+++ createSchema.sql
@@ -1,9 +1,55 @@
-CREATE TABLE catalogs(id int unsigned not null auto_increment primary key, name varchar(255), UNIQUE INDEX (name));
+# Таблица каталогов
+CREATE TABLE catalogs
+(
+    id   int unsigned not null auto_increment primary key,
+    name varchar(255) not null,
+    UNIQUE INDEX (name)
+);
 
-CREATE TABLE sections(id int unsigned not null AUTO_INCREMENT PRIMARY KEY, name varchar(255) not null, description varchar(255) not null, catalog_id int unsigned not null, FOREIGN KEY(catalog_id) REFERENCES catalogs(id), UNIQUE INDEX (name));
+# Таблица разделов, отношение один ко многим с каталогом
+# (один каталог содержит много разделов)
+CREATE TABLE sections
+(
+    id          int unsigned not null AUTO_INCREMENT PRIMARY KEY,
+    name        varchar(255) not null,
+    description varchar(255) not null,
+    catalog_id  int unsigned not null,
+    FOREIGN KEY (catalog_id) REFERENCES catalogs (id),
+    UNIQUE INDEX (name)
+);
 
-CREATE TABLE products(id int unsigned not null auto_increment primary key , name varchar(255) not null,  is_active boolean not null, price smallint unsigned, description varchar(255) not null, discount tinyint unsigned, promo_discount tinyint unsigned, UNIQUE INDEX (name));
+# Таблица товаров
+CREATE TABLE products
+(
+    id              int unsigned      not null auto_increment primary key,
+    name            varchar(255)      not null,
+    main_section_id int unsigned      not null, # ссылка на основной раздел товара
+    is_active       boolean           not null, # товар может быть неактивным
+    price           smallint unsigned not null,
+    description     varchar(255)      not null,
+    discount        tinyint unsigned  not null, # скидка для товара
+    promo_discount  tinyint unsigned  not null, # скидка с учетом промокода
+    UNIQUE INDEX (name, main_section_id),       # имя товара уникальное в пределах основного раздела
+    FOREIGN KEY (main_section_id) REFERENCES sections (id)
+);
 
-CREATE TABLE productImages(id int unsigned not null auto_increment primary key , product_id int unsigned not null , link varchar(255) not null , is_main boolean not null,  foreign key (product_id) references products(id));
+# Таблица, хранящая ссылки на изображения для определенного товара
+CREATE TABLE productImages
+(
+    id         int unsigned not null auto_increment primary key,
+    product_id int unsigned not null,
+    link       varchar(255) not null,
+    is_main    boolean      not null, # главное изображение(картинка для анонса)
+    foreign key (product_id) references products (id)
+);
 
-CREATE TABLE sectionsProducts(id int unsigned not null auto_increment primary key, product_id int unsigned not null , section_id int unsigned not null , foreign key (product_id) references products(id), foreign key (section_id) references sections(id));
+# Таблица связи товаров и разделов. Используется отношение многие ко многим, тк товар
+# может содержаться в нескольких разделах
+CREATE TABLE sectionsProducts
+(
+    id         int unsigned not null auto_increment primary key,
+    product_id int unsigned not null,
+    section_id int unsigned not null,
+    foreign key (product_id) references products (id),
+    foreign key (section_id) references sections (id)
+);
--- insertData.sql
+++ insertData.sql
@@ -1,18 +1,34 @@
-INSERT INTO catalogs(name) VALUES ('Смартфоны и гаджеты'), ('Ноутбуки и компьютеры');
+INSERT INTO catalogs(name)
+VALUES ('Смартфоны и гаджеты'),
+       ('Ноутбуки и компьютеры');
 
-INSERT INTO sections(name, description, catalog_id) VALUES ('Смартфоны, мобильные телефоны','описание раздела сматрфонов',1),
-                                                           ('Планшеты и электронные книги','описание раздела',1),
-                                                           ('Ноутбуки','описание раздела ноутбуков',2),
-                                                           ('Компьютеры и мониторы','описание раздела',2);
-
-
-INSERT INTO products(name, is_active, price, description, discount, promo_discount) VALUES
-                                                                                        ('IPhone', false, 1000,'iphone description', 10,10),
-                                                                                        ('Samsung', true, 500,'samsung description', 15,15),
-                                                                                        ('Huawei', true, 3000,'huawei laptop description', 20,20),
-                                                                                        ('Mac', true, 5000,'mac description', 40,40);
+INSERT INTO sections(name, description, catalog_id)
+VALUES ('Смартфоны, мобильные телефоны', 'описание раздела сматрфонов', 1),
+       ('Планшеты и электронные книги', 'описание раздела', 1),
+       ('Ноутбуки', 'описание раздела ноутбуков', 2),
+       ('Компьютеры и мониторы', 'описание раздела', 2);
 
 
-INSERT INTO sectionsProducts( product_id, section_id) values (1,1), (2,1),(3,2),(4,2),(3,4),(4,4);
+INSERT INTO products(name, is_active, price, description, discount, promo_discount, main_section_id)
+VALUES ('IPhone', false, 1000, 'iphone description', 10, 10, 1),
+       ('Samsung', true, 500, 'samsung description', 15, 15, 1),
+       ('Huawei', true, 500, 'samsung description', 15, 15, 1),
+       ('Xiaomi', true, 500, 'samsung description', 15, 15, 1),
+       ('Samsung', true, 500, 'samsung description', 15, 15, 3),
+       ('Huawei', true, 3000, 'huawei laptop description', 20, 20, 3),
+       ('Mac', true, 5000, 'mac description', 40, 40, 3),
+       ('HP', true, 5000, 'hp description', 40, 40, 4),
+       ('Samsung', true, 5000, 'samsung description', 40, 40, 4);
 
-INSERT INTO productImages(product_id, link, is_main) VALUES (4,'main.jpg', true), (4,'second.jpg', false),(4,'third.jpg', false);
+
+
+INSERT INTO sectionsProducts(product_id, section_id)
+values (1, 1),
+       (2, 1),
+       (8, 3),
+       (8, 4);
+
+INSERT INTO productImages(product_id, link, is_main)
+VALUES (4, 'main.jpg', true),
+       (4, 'second.jpg', false),
+       (4, 'third.jpg', false);
