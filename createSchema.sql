23fb8f41ccc39e937ad99d51bc13a0622ac35f16
egor <egorka.leps@mail.ru>
2022-07-22 07:04:38 +0700

    schema updated

--- createSchema.sql
+++ createSchema.sql
@@ -18,29 +18,41 @@ CREATE TABLE sections
     UNIQUE INDEX (name)
 );
 
+
+# Таблица, хранящая ссылки на изображения
+CREATE TABLE images
+(
+    id   int unsigned not null auto_increment primary key,
+    link varchar(255) not null,
+    alt  varchar(255) not null
+);
+
+
 # Таблица товаров
 CREATE TABLE products
 (
     id              int unsigned      not null auto_increment primary key,
     name            varchar(255)      not null,
     main_section_id int unsigned      not null, # ссылка на основной раздел товара
+    main_image_id   int unsigned      not null, # ссылка на основное изображение товара
     is_active       boolean           not null, # товар может быть неактивным
     price           smallint unsigned not null,
     description     varchar(255)      not null,
-    discount        tinyint unsigned  not null, # скидка для товара
-    promo_discount  tinyint unsigned  not null, # скидка с учетом промокода
+    discount_price  smallint unsigned not null, # цена с учетом скидки
+    promo_price     smallint unsigned not null, # цена с учетом промокода
     UNIQUE INDEX (name, main_section_id),       # имя товара уникальное в пределах основного раздела
-    FOREIGN KEY (main_section_id) REFERENCES sections (id)
+    FOREIGN KEY (main_section_id) REFERENCES sections (id),
+    FOREIGN KEY (main_image_id) REFERENCES images (id)
 );
 
-# Таблица, хранящая ссылки на изображения для определенного товара
-CREATE TABLE productImages
+# Таблиуа связи товаров и изображений(многие ко многим)
+CREATE TABLE productsImages
 (
-    id         int unsigned not null auto_increment primary key,
-    product_id int unsigned not null,
-    link       varchar(255) not null,
-    is_main    boolean      not null, # главное изображение(картинка для анонса)
-    foreign key (product_id) references products (id)
+    id         INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    image_id   INT UNSIGNED NOT NULL,
+    product_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (image_id) REFERENCES images (id),
+    FOREIGN KEY (product_id) REFERENCES products (id)
 );
 
 # Таблица связи товаров и разделов. Используется отношение многие ко многим, тк товар
--- insertData.sql
+++ insertData.sql
@@ -8,17 +8,21 @@ VALUES ('Смартфоны, мобильные телефоны', 'описан
        ('Ноутбуки', 'описание раздела ноутбуков', 2),
        ('Компьютеры и мониторы', 'описание раздела', 2);
 
+INSERT INTO images(link, alt)
+VALUES ('main.png','default.png'),
+       ('second.png', 'default.png'),
+       ('third.png', 'default.png');
 
-INSERT INTO products(name, is_active, price, description, discount, promo_discount, main_section_id)
-VALUES ('IPhone', false, 1000, 'iphone description', 10, 10, 1),
-       ('Samsung', true, 500, 'samsung description', 15, 15, 1),
-       ('Huawei', true, 500, 'samsung description', 15, 15, 1),
-       ('Xiaomi', true, 500, 'samsung description', 15, 15, 1),
-       ('Samsung', true, 500, 'samsung description', 15, 15, 3),
-       ('Huawei', true, 3000, 'huawei laptop description', 20, 20, 3),
-       ('Mac', true, 5000, 'mac description', 40, 40, 3),
-       ('HP', true, 5000, 'hp description', 40, 40, 4),
-       ('Samsung', true, 5000, 'samsung description', 40, 40, 4);
+INSERT INTO products(name, is_active, price, description, discount_price, promo_price, main_section_id, main_image_id)
+VALUES ('IPhone', false, 1000, 'iphone description', 700, 600, 1, 1),
+       ('Samsung', true, 500, 'samsung description', 400, 300, 1, 1),
+       ('Huawei', true, 500, 'samsung description', 400, 400, 1, 1),
+       ('Xiaomi', true, 500, 'samsung description', 499, 400, 1, 1),
+       ('Samsung', true, 500, 'samsung description', 400, 400, 3, 1),
+       ('Huawei', true, 3000, 'huawei laptop description', 2000, 2500, 3, 1),
+       ('Mac', true, 5000, 'mac description', 4000, 4000, 3, 1),
+       ('HP', true, 5000, 'hp description', 4000, 4000, 4, 1),
+       ('Samsung', true, 5000, 'samsung description', 4000, 4000, 4, 1);
 
 
 
@@ -28,7 +32,11 @@ values (1, 1),
        (8, 3),
        (8, 4);
 
-INSERT INTO productImages(product_id, link, is_main)
-VALUES (4, 'main.jpg', true),
-       (4, 'second.jpg', false),
-       (4, 'third.jpg', false);
+
+INSERT INTO productsImages(image_id, product_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (2, 1),
+       (2, 2),
+       (2, 3);
