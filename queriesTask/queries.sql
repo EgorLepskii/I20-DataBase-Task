77b411a1c520ddf1839c9bbef527ac2188b9f4de
egor <egorka.leps@mail.ru>
2022-07-27 17:58:27 +0700

    query for category component updated

--- queriesTask/queries.sql
+++ queriesTask/queries.sql
@@ -1,29 +1,29 @@
 # Вывод названия раздела и количества товаров в нем.
 SELECT s.id, s.name, COUNT(sP.id) as products_count
 FROM sections s
-          LEFT JOIN sectionsProducts sP on s.id = sP.section_id
+         LEFT JOIN sectionsProducts sP on s.id = sP.section_id
 GROUP BY s.id
 HAVING products_count > 0
 ORDER BY products_count desc;
 
 
 # Детальная страница раздела.
-SELECT DISTINCT s.id              as section_id,
-                s.name            as category_name,
-                s.description,
-                p.id              as product_id,
-                p.name,
-                p.main_section_id as main_section_id,
-                s2.name           as main_section_name,
-                i.link            as main_image_link,
-                i.alt             as alt
+
+SELECT s.name,
+       s.description
+FROM sections s WHERE s.id = 1;
+
+SELECT p.id              as product_id,
+       p.name,
+       p.main_section_id as main_section_id,
+       s.name           as main_section_name,
+       i.link            as main_image_link,
+       i.alt             as alt
 FROM sectionsProducts sP
-         JOIN sections s on s.id = sP.section_id
          JOIN products p on p.id = sP.product_id
-         JOIN productsImages pI on p.id = pI.product_id
          JOIN images i on i.id = p.main_image_id
-         JOIN sections s2 on s2.id = p.main_section_id
-WHERE s.id = 1
+         JOIN sections s on s.id = p.main_section_id
+WHERE sP.section_id = 1
   AND p.is_active = true
 LIMIT 12 OFFSET 0;
 
@@ -36,7 +36,8 @@ SELECT p.name,
        p.discount_price,
        p.main_image_id,
        p.main_section_id
-FROM products p WHERE p.id = 1;
+FROM products p
+WHERE p.id = 1;
 
 # Получение разделов товара
 SELECT s.id, s.name
